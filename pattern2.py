f8b01db2d92a591260a712006af0b7a0cac8d2f1
Tarun kumar <tarun.dsvv@gmail.com>
2024-08-23 22:21:03 +0530

    pattern2 code edited

--- pattern2.py
+++ pattern2.py
@@ -1,1 +1,7 @@
-print( 2+7)
+for i in range(1,5,1):
+    print(' '*(4-i)+'*'*i)
+    #OR
+    # 1). print(' '*(4-i), end=' ')
+    #     print('*'*i)
+    #2). for i in range(1,5,1):
+    #       print(' '*(4-i),'*'*i,sep='')
