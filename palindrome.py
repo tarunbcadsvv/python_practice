ab78b61aea50f89766d324524d976986de952be9
Tarun kumar <tarun.dsvv@gmail.com>
2024-08-25 23:33:07 +0530

    code added for palindrome num

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+n= int(input('enter a number which you want to check palindrome:'))
+rev=0
+num=n
+while n>0:
+    digit = n%10
+    rev = rev*10+digit
+    n=n//10
+if num==rev:
+    print('palindrome number!')
+else:
+    print('not a palindrome number!')
