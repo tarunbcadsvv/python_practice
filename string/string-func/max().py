0d4267c76f33b4bd4d41c165b5311e56c789f571
tarun <133577577+tarunbcadsvv@users.noreply.github.com>
2024-08-27 14:42:58 +0530

    string functions code added

---
+++ string/string-func/max().py
@@ -0,0 +1,2 @@
+s1=input('enter a string:')
+print(max(s1))    #this function gives maximum numeric value character
---
+++ string/string-func/min().py
@@ -0,0 +1,2 @@
+s1=input('enter a string:')
+print(min(s1))    #this function gives minimum numeric value character
